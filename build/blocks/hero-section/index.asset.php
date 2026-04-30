36dbe05a7d38a04519098ea11847a2604150a9fd
Albert A. <albert@mybizniche.com>
2026-04-30 16:42:44 +0800

    Update asset versions and clean up CSS comments for donation, hero, intro, footer, and navbar blocks

--- build/blocks/hero-section/index.asset.php
+++ build/blocks/hero-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '48590346e0c024f92a69');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '84c4d390742142dcef3c');
--- build/blocks/site-navbar/index.asset.php
+++ build/blocks/site-navbar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'a117f165c9dba392e094');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'cefa5099af847641de0d');
